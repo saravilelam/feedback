39f6bc8a3416b56725ca810710a238bc7beb4fdf
saravilelambizerra <saravilelambizerra@gmail.com>
2025-03-28 04:33:54 +0000

    ajuste

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ export function App() {
         <input type="button" value={5} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white sm:w-13 sm:h-13 hover:bg-orange hover:text-white hover: cursor-pointer" onClick={() => handleMudarNotaAvaliacao(5)} />
       </div>
 
-      <button onClick={handleSubmit} className="bg-orange w-full uppercase tracking-1 font-bold rounded-3xl text-sm py-3 hover: bg-white text-orange hover: cursor-pointer">submit</button>
+      <button onClick={handleSubmit} className="bg-orange w-full uppercase tracking-1 font-bold rounded-3xl text-sm py-3 hover:bg-white hover:text-orange hover: cursor-pointer">submit</button>
       
     </div>
     ) : (
