4cdf2c97bbcc8411f0d63e328cf1b60a21c8ae4b
saravilelambizerra <saravilelambizerra@gmail.com>
2025-03-28 12:38:22 +0000

    ajuste marcio

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ export function App() {
       <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass max-w-103">
         <img className="mx-auto mb-6" src={illustrationThankYou} alt="illustration Thank You" />
 
-        <p className="text-orange bg-dark-blue w-fit mx-auto px- 3 py-1.25 rounded-3xl mb-6 ">You selected {notaAvaliacao} out of 5 </p>
+        <p className="text-orange bg-dark-blue w-fit mx-auto px-3 py-1.25 rounded-3xl mb-6 ">You selected {notaAvaliacao} out of 5 </p>
 
         <h1 className="text-2xl font-bold mb-2.5 text-center ">Thank You</h1>
 
