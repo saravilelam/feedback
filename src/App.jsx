15bc59f1000865f297bca0a369e89660ebbe1bf7
saravilelambizerra <saravilelambizerra@gmail.com>
2025-03-28 04:22:26 +0000

    fim da criaçao e estilização seg card

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import iconStar from "./assets/icon-star.svg"
+import illustrationThankYou from "./assets/illustration-thank-you.svg"
 import { useState } from "react"
+
 
 export function App() {
 
@@ -45,7 +47,13 @@ export function App() {
     </div>
     ) : (
       <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass">
-        <p>{notaAvaliacao}</p>
+        <img className="mx-auto mb-6" src={illustrationThankYou} alt="illustration Thank You" />
+
+        <p className="text-orange bg-dark-blue w-fit mx-auto px- 3 py-1.25 rounded-3xl mb-6 ">You selected {notaAvaliacao} out of 5 </p>
+
+        <h1 className="text-2xl font-bold mb-2.5 text-center ">Thank You</h1>
+
+        <p className="text-sm text-light-grey leading-1 text-center ">We appreciate you taking the time to give a rating. If you ever need more support, don´t hesitate to get in touch!</p>
       </div>
       )
     )
