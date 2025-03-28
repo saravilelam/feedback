257863ff6b6ca0b76710c21ad6bd9fda802d1244
saravilelambizerra <saravilelambizerra@gmail.com>
2025-03-28 03:47:07 +0000

    troca de card

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,9 @@ export function App() {
     setNotaAvaliacao(nota)
   }
 
-  return(
-    <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass">
+  return (
+    notaAvaliacao === 0 ? (
+      <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass">
       <div className="bg-dark-blue w-fit p-4 rounded-full rounded-2xl mb-4">
         <img src={iconStar} alt="icon star" />
       </div>
@@ -32,5 +33,10 @@ export function App() {
       <button className="bg-orange w-full uppercase tracking-1 font-bold rounded-3xl text-sm py-3 ">submit</button>
       
     </div>
-  )
+    ) : (
+      <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass">
+        <p>{notaAvaliacao}</p>
+      </div>
+      )
+    )
 }
